5a894b008d661042750826742b26a916cf501690
Islam Elgueniari <islam.gueniari@gmail.com>
2023-01-26 23:04:57 +0100

    About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {Fragment} from 'react'
+import { useNavigate } from 'react-router'
 
 export default function About() {
+
+  const navigate = useNavigate();
+
   return (
-    <div className='w-full h-screen absolute p-8 md:ml-8'>
-      <h1 className="about-title mt-24 text-4xl md:text-5xl font-bold text-slate-200">About me:</h1>
-      <p className='text-2xl mt-10 text-slate-300 bg-gray-900 rounded-xl p-6'>Hello, <br />My name Is <span className='text-sky-400'>Achraf</span>, a front-end ReactJs web developer, designer and content creator since 2021 from Morocco,</p>
+    <div className='w-full h-screen absolute p-6 overflow-hidden'>
+      <div className='circle bg-gray-900 absolute w-full h-full'></div>
+      <h1 className="about-title mt-24 text-4xl md:text-5xl font-bold text-slate-200 md:ml-8">About me:</h1>
+      <p className='description text-2xl mt-10 text-slate-300 bg-gray-900 rounded-xl p-6 md:w-2/3 mx-auto'>Hello, <br />My name Is <span className='text-sky-400'>Achraf</span>, a front-end ReactJs web developer, designer and content creator since 2021 from Morocco. I am also interested in A.I, Linux kernel and cryptocurrencies.</p>
+      <button className='CTC absolute mt-20 w-auto bg-sky-500 p-2 rounded-full text-slate-200 font-bold text-2xl right-12 md:mt-28 md:right-20 lg:text-3xl' onClick={() => {navigate("/")}}>Go back home</button>
+      
     </div>
   )
 }

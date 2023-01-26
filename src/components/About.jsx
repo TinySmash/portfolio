6d0a76191f5f25b6f3d05720388d3c6ad2e40e08
Islam Elgueniari <islam.gueniari@gmail.com>
2023-01-26 23:42:47 +0100

    About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ export default function About() {
       <div className='circle bg-gray-900 absolute w-full h-full'></div>
       <h1 className="about-title mt-24 text-4xl md:text-5xl font-bold text-slate-200 md:ml-8">About me:</h1>
       <p className='description text-2xl mt-10 text-slate-300 bg-gray-900 rounded-xl p-6 md:w-2/3 mx-auto'>Hello, <br />My name Is <span className='text-sky-400'>Achraf</span>, a front-end ReactJs web developer, designer and content creator since 2021 from Morocco. I am also interested in A.I, Linux kernel and cryptocurrencies.</p>
-      <button className='CTC absolute w-auto bg-sky-500 p-2 rounded-full text-slate-200 font-bold text-2xl bottom-10 right-12 md:bottom-10 md:right-20 lg:text-3xl' onClick={() => {navigate("/")}}>Go back home</button>
+      <button className='CTC absolute w-auto bg-sky-500 p-2 rounded-full text-slate-200 font-bold text-2xl bottom-10 right-12 md:bottom-10 md:right-20 md:text-3xl lg:text-4xl' onClick={() => {navigate("/")}}>Go back home</button>
       
     </div>
   )

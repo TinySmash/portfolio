85bdd1e09420e9bf4ffe970df651bde2c9e57bd7
Islam Elgueniari <islam.gueniari@gmail.com>
2023-02-13 22:36:38 +0100

    Skills Done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
-import React, {Fragment} from 'react'
+import React from 'react'
 import { useNavigate } from 'react-router'
+import achrafProfile from '../images/achrafProfile.jpeg'
 
 export default function About() {
 
@@ -10,8 +11,10 @@ export default function About() {
       <div className='circle bg-gray-900 absolute w-full h-full'></div>
       <h1 className="about-title mt-24 text-4xl md:text-5xl font-bold text-slate-200 md:ml-8">About me:</h1>
       <p className='description text-2xl mt-10 text-slate-300 bg-gray-900 rounded-xl p-6 md:w-2/3 mx-auto'>Hello, <br />My name Is <span className='text-sky-400'>Achraf</span>, a front-end ReactJs web developer, designer and content creator since 2021 from Morocco. I am also interested in A.I, Linux kernel and cryptocurrencies.</p>
-      <button className='CTC absolute w-auto bg-sky-500 p-2 rounded-full text-slate-200 font-bold text-2xl bottom-10 right-12 md:bottom-10 md:right-20 md:text-3xl lg:text-4xl' onClick={() => {navigate("/")}}>Go back home</button>
-      
+      <button className='CTC absolute w-auto bg-sky-500 p-2 rounded-full text-slate-200 font-bold text-2xl bottom-10 right-12 md:right-20 md:text-3xl lg:text-4xl' onClick={() => {navigate("/")}}>Go back home</button>
+      <div className='profile absolute w-1/3 sm:w-1/4 md:w-1/5 lg:w-1/6 rounded-full border-2 border-gray-900 bg-gray-900 p-1'>
+        <img src={achrafProfile} loading='lazy' alt="" className='profilePic relative rounded-full '/>
+      </div>
     </div>
   )
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import { useNavigate } from 'react-router';
+import { useInView } from 'react-intersection-observer';
 import ProgressBar from '../ProgressBar'
 import influencer from '../images/influencer.png'
 import coding from '../images/coding.png'
 import writing from '../images/writing.png'
 import vector from '../images/vector.png'
+import github from '../images/github.png';
 import p1 from '../images/project-1.png'
 import p2 from '../images/project-2.png'
 import p3 from '../images/project-3.png'
+
+
+
 export default function Skills() {
+
+  const navigate = useNavigate();
 
   const skillsArray = [
     {name : "HTML & CSS", percentage: 85, id: 1},
     {name : "Javascript", percentage: 75, id: 2},
     {name : "Responsive web design", percentage: 85, id: 3},
     {name : "Tailwindcss", percentage: 65, id: 4},
-    {name : "ReactJs & Redux", percentage: 70, id: 5},
+    {name : "ReactJs & Redux", percentage: 80, id: 5},
   ]
 
   const features = ["create", "build", "design", "make", "spark"]
@@ -43,9 +51,24 @@ export default function Skills() {
   }, 2000)
 
 
+  // APPEARING DESIGNS
+
+  const [ ref, inView ] = useInView({
+    triggerOnce: true
+  });
+
+  const triggerDesign = () => {
+    const designs = document.querySelectorAll('.skilldes');
+    designs.forEach((e) => {
+      e?.classList?.add('myWebDesign');
+    })
+  }
+
+
   return (
     <div className='w-full h-full'>
         <section className='skills skill-container w-full h-auto sm:h-screen pb-12 pt-24 px-3 flex flex-wrap justify-center'>
+          <h1 className='text-3xl sm:text-6xl font-bold text-slate-200 mb-6'>These are my main technologies & skills</h1>
           {skillsArray.map((e) => {
             return (
               <div className='relative w-48 lg:w-1/5 h-32 mx-2 my-4 px-5 pb-2 pt-3 rounded-xl bg-sharp-glass flex justify-center'>
@@ -65,22 +88,22 @@ export default function Skills() {
           
           <ul className='list-none flex flex-wrap w-full h-auto justify-center'>
 
-            <li className="skilldesc w-2/3 md:w-1/3 h-auto md:h-80 border-2 border-sky-600 mr-4 mb-6 bg-slate-700 rounded-3xl p-3">
+            <li className="skilldesc w-2/3 md:w-[45%] h-auto md:h-80 border-2 border-sky-600 mr-4 mb-6 bg-slate-700 rounded-3xl p-3">
               <img src={coding} alt="" className='w-1/4 mx-auto mt-2 mb-2'  loading='lazy'/>
               <h2 className='text-slate-200 text-2xl font-semibold text-center'>Web developer</h2>
               <p className='text-slate-300'>As a web developer with over 2 years of experience, I have a passion for creating visually appealing and user-friendly websites that deliver an exceptional online experience to visitors. Using technologies like HTML, CSS, Tailwindcss, and ReactJs.</p>
             </li>
-            <li className="skilldesc w-2/3 md:w-1/3 h-auto md:h-80 border-2 border-sky-600 mr-4 mb-6 bg-slate-700 rounded-3xl p-3">
+            <li className="skilldesc w-2/3 md:w-[45%] h-auto md:h-80 border-2 border-sky-600 mr-4 mb-6 bg-slate-700 rounded-3xl p-3">
               <img src={vector} alt="" className='w-1/4 mx-auto mt-2 mb-2'  loading='lazy'/>
               <h2 className='text-slate-200 text-2xl font-semibold text-center'>Web designer</h2>
-              <p className='text-slate-300'>I have a passion for creating visually stunning and aesthetically pleasing websites that engage and impress visitors. My expertise lies in the use of design tools such as Figma for creating website templates, and I have a track record of delivering high-quality design solutions that meet clients' needs and requirements.</p>
+              <p className='text-slate-300'>I am passionate about creating visually stunning and pleasing websites that engage and impress visitors. I use design tools such as Figma for creating templates and visual content, my responsibility is delivering high-quality solutions that meet clients' needs</p>
             </li>
-            <li className="skilldesc w-2/3 md:w-1/3 h-auto md:h-80 border-2 border-sky-600 mr-4 mb-6 bg-slate-700 rounded-3xl p-3">
+            <li className="skilldesc w-2/3 md:w-[45%] h-auto md:h-80 border-2 border-sky-600 mr-4 mb-6 bg-slate-700 rounded-3xl p-3">
               <img src={writing} alt="" className='w-1/4 mx-auto mt-2 mb-2'  loading='lazy'/>
               <h2 className='text-slate-200 text-2xl font-semibold text-center'>Content writer</h2>
               <p className='text-slate-300'>I am an content creator on instagram, I like to write and post my coding journey in form of Instagram reels and stories, I also write website content to optimize it for search engines and rank it higher to drive more website traffic</p>
             </li>
-            <li className="skilldesc w-2/3 md:w-1/3 h-auto md:h-80 border-2 border-sky-600 mr-4 mb-6 bg-slate-700 rounded-3xl p-3">
+            <li className="skilldesc w-2/3 md:w-[45%] h-auto md:h-80 border-2 border-sky-600 mr-4 mb-6 bg-slate-700 rounded-3xl p-3">
               <img src={influencer} alt="" className='w-1/4 mx-auto mt-2 mb-2'  loading='lazy'/>
               <h2 className='text-slate-200 text-2xl font-semibold text-center'>Content creator</h2>
               <p className='text-slate-300'>I am also an active content creator on instagram. Join a community of more than 120k people passionate about coding and programming. I'm sharing content about my coding journey, other people experiences, coding tips funny reels, and more. <a href="https://www.instagram.com/achrafcodes" className='font-bold'>@achrafcodes</a></p>
@@ -88,18 +111,26 @@ export default function Skills() {
           </ul>
           <h1 className='text-2xl sm:text-5xl font-bold text-slate-200 mb-6'>I can build stunning Web interfaces within smart web applications.</h1>
         </section>
-        <section className='skills w-full h-auto pb-12 pt-24 px-7'>
-          <ul className='relative block w-full h-auto py-10 px-5 list-none '>
-            <li className="skilldesc w-full md:w-2/3 h-auto border-2 border-sky-600 mb-6 rounded-md p-1 myWebDesign">
+        <section className='skills w-full h-auto pb-12 pt-24 px-7' >
+          <ul className='relative block w-full h-auto py-10 px-5 list-none ' ref={ref}>
+          {inView && triggerDesign()}
+            <li className={`skilldes w-full md:w-2/3 h-auto border-2 border-sky-600 mb-6 rounded-md p-1`}>
               <img src={p1} alt="" loading="lazy"></img>
             </li>
-            <li className="skilldesc w-full md:w-2/3 h-auto border-2 border-sky-600 mb-6 rounded-md p-1 myWebDesign">
+            <li className={`skilldes w-full md:w-2/3 md:ml-[14%] h-auto border-2 border-sky-600 mb-6 rounded-md p-1`}>
             <img src={p2} alt="" loading="lazy"></img>
             </li>
-            <li className="skilldesc w-full md:w-2/3 h-auto border-2 border-sky-600 rounded-md p-1 myWebDesign">
+            <li className={`skilldes w-full md:w-2/3 md:ml-[28%] h-auto border-2 border-sky-600 rounded-md p-1`} >
             <img src={p3} alt="" loading="lazy"></img>
             </li>
           </ul>
+          <div className='block md:flex w-full h-auto items-center'>
+            <h1 className='text-3xl sm:text-6xl font-bold text-slate-200 mb-6'>Want more ? check my GitHub profile.</h1>
+            <div className='sm-icon bg-slate-900 w-fit p-4 rounded-full m-auto'>
+              <a href="https://github.com/Achrafcodes" target="_blank" ><img src={github} loading="lazy" alt="" className='w-24'></img></a>
+            </div>
+          </div>
+          <button className='CTC relative w-auto bg-sky-500 p-2 rounded-full text-slate-200 font-bold text-2xl -bottom-6  md:text-3xl lg:text-4xl ml-auto' onClick={() => {navigate("/")}}>Go back home</button>
         </section>
     </div>
   )

1d1bab52bbe5123fe4083b3f21bb988020385b6c
TinySmash <islam.gueniari@gmail.com>
2023-04-04 20:45:49 +0000

    first section lets work

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -90,7 +90,7 @@ function Hero() {
       <section className='Hero w-full h-screen px-5 flex justify-center'>
         <img src={avatar} alt="" className='w-1/2 sm:w-1/3 md:w-1/4 mt-24 sm:mt-36 md:mt-32 lg:mt-28 absolute right-5 sm:right-7 md:right-14 bg-slate-400 rounded-full p-2'/>
         <div className='absolute left-8 mt-[23rem] sm:mt-44 text-3xl sm:text-4xl md:text-5xl lg:text-6xl '>
-          <h1 className='title font-semibold text-slate-200 static-typing-border pr-1 mb-5 w-auto max-w-3/4 sm:max-w-1/2 md:max-w-2/3' ref={header}><span ref={title}></span> <span className='nameTitle text-sky-400' ref={Name}></span></h1>
+          <h1 className='title font-medium text-slate-200 static-typing-border pr-1 mb-5 w-auto max-w-3/4 sm:max-w-1/2 md:max-w-2/3' ref={header}><span ref={title}></span> <span className='nameTitle text-sky-400' ref={Name}></span></h1>
           <h1 className='absolute w-[120%] text-sky-400 text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold pr-1 pText' ref={pText}></h1>
         </div>
         <div className='absolute flex w-3/4 sm:w-1/2 h-20 sm:h-24 md:h-28 bottom-8'>
--- src/components/LetsWork.jsx
+++ src/components/LetsWork.jsx
@@ -1,31 +1,58 @@
-import React, { Component } from 'react'
-import lp from '../images/LandingPage.png'
-import websites from '../images/websites.png'
-import webapp from '../images/webapp.png'
+import React from 'react';
+import lp from '../images/LandingPage.png';
+import websites from '../images/websites.png';
+import webapp from '../images/webapp.png';
 
-export default class LetsWork extends Component {
-  render() {
-    return (
-      <div className='w-full h-auto min-h-screen bg-sharp-glass absolute p-4'>
-        <section className='w-full h-auto min-h-screen relative p-2'>
-            <h1 className='mt-16 text-center relative text-slate-200 text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold'>Building Beautiful Websites - Let's Do This!</h1>
-            <h2 className='mt-5 text-center relative text-slate-200 text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold'>I can build</h2>
-            <ul className='list-none flex flex-wrap w-full h-auto justify-center mt-5'>
-              <li className="skilldesc w-2/3 md:w-[30%] h-auto border-2 border-sky-600 mr-4 mb-6 bg-slate-700 rounded-3xl p-3">
-                <img src={lp} alt="" className='w-1/3 mx-auto mt-2 mb-2'  loading='lazy'/>
-                <h2 className='text-slate-200 text-2xl font-semibold text-center'>Landing pages</h2>
-              </li>
-              <li className="skilldesc w-2/3 md:w-[30%] h-auto border-2 border-sky-600 mr-4 mb-6 bg-slate-700 rounded-3xl p-3">
-                <img src={websites} alt="" className='w-1/3 mx-auto mt-2 mb-2'  loading='lazy'/>
-                <h2 className='text-slate-200 text-2xl font-semibold text-center'>Websites</h2>
-              </li>
-              <li className="skilldesc w-2/3 md:w-[30%] h-auto border-2 border-sky-600 mr-4 mb-6 bg-slate-700 rounded-3xl p-3">
-                <img src={webapp} alt="" className='w-1/3 mx-auto mt-2 mb-2'  loading='lazy'/>
-                <h2 className='text-slate-200 text-2xl font-semibold text-center'>Web applications</h2>
-              </li>
-            </ul>
-        </section>
-      </div>
-    )
-  }
+export default function LetsWork() {
+  return (
+    <div className="w-full h-auto min-h-screen bg-sharp-glass absolute p-4">
+      <section className="w-full h-auto min-h-screen relative p-2">
+        <h1 className="mt-16 text-center relative text-slate-200 text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold">
+          Building Beautiful Websites - Let's Do This!
+        </h1>
+        <h2 className="mt-5 text-center relative text-slate-200 text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold">
+          I can build
+        </h2>
+        <ul className="list-none flex flex-wrap w-full h-auto justify-center mt-5 off-app mb-28">
+          <li className="skilldesc w-2/3 md:w-[30%] h-auto border-2 border-sky-600 mr-4 mb-6 bg-slate-700 rounded-3xl p-3">
+            <img
+              src={lp}
+              alt=""
+              className="w-1/3 mx-auto mt-2 mb-2"
+              loading="lazy"
+            />
+            <h2 className="text-slate-200 text-2xl font-semibold text-center">
+              Landing pages
+            </h2>
+          </li>
+          <li className="skilldesc w-2/3 md:w-[30%] h-auto border-2 border-sky-600 mr-4 mb-6 bg-slate-700 rounded-3xl p-3">
+            <img
+              src={websites}
+              alt=""
+              className="w-1/3 mx-auto mt-2 mb-2"
+              loading="lazy"
+            />
+            <h2 className="text-slate-200 text-2xl font-semibold text-center">
+              Websites
+            </h2>
+          </li>
+          <li className="skilldesc w-2/3 md:w-[30%] h-auto border-2 border-sky-600 mr-4 mb-6 bg-slate-700 rounded-3xl p-3">
+            <img
+              src={webapp}
+              alt=""
+              className="w-1/3 mx-auto mt-2 mb-2"
+              loading="lazy"
+            />
+            <h2 className="text-slate-200 text-2xl font-semibold text-center">
+              Web applications
+            </h2>
+          </li>
+        </ul>
+        <h2 className=" text-center absolute bottom-5 text-slate-200 text-2xl sm:text-2xl md:text-3xl lg:text-4xl font-semibold">
+          Working with you, we'll create an outstanding web presence. Let's get
+          started!
+        </h2>
+      </section>
+    </div>
+  );
 }

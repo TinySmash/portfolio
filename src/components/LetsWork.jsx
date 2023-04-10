b0783a013eb2a2a9cf40de237c4ad666ac045944
TinySmash <islam.gueniari@gmail.com>
2023-04-10 00:50:28 +0000

    2nd let's work section

--- src/components/LetsWork.jsx
+++ src/components/LetsWork.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import lp from '../images/LandingPage.png';
 import websites from '../images/websites.png';
 import webapp from '../images/webapp.png';
+import avatar from '../images/achraf.png';
 
 export default function LetsWork() {
   return (
@@ -14,7 +15,7 @@ export default function LetsWork() {
           I can build
         </h2>
         <ul className="list-none flex flex-wrap w-full h-auto justify-center mt-5 off-app mb-28 z-50">
-          <li className="skilldesc w-2/3 md:w-[30%] h-auto border-2 border-sky-600 mr-4 mb-6 bg-slate-700 rounded-3xl p-3 z-50">
+          <li className="skilldesc w-2/3 md:w-[30%] h-auto border-2 border-sky-600 mr-4 mb-6 bg-slate-700 rounded-3xl p-3 z-40">
             <img
               src={lp}
               alt=""
@@ -25,7 +26,7 @@ export default function LetsWork() {
               Landing pages
             </h2>
           </li>
-          <li className="skilldesc w-2/3 md:w-[30%] h-auto border-2 border-sky-600 mr-4 mb-6 bg-slate-700 rounded-3xl p-3 z-50">
+          <li className="skilldesc w-2/3 md:w-[30%] h-auto border-2 border-sky-600 mr-4 mb-6 bg-slate-700 rounded-3xl p-3 z-40">
             <img
               src={websites}
               alt=""
@@ -36,7 +37,7 @@ export default function LetsWork() {
               Websites
             </h2>
           </li>
-          <li className="skilldesc w-2/3 md:w-[30%] h-auto border-2 border-sky-600 mr-4 mb-6 bg-slate-700 rounded-3xl p-3 z-50">
+          <li className="skilldesc w-2/3 md:w-[30%] h-auto border-2 border-sky-600 mr-4 mb-6 bg-slate-700 rounded-3xl p-3 z-40">
             <img
               src={webapp}
               alt=""
@@ -52,6 +53,19 @@ export default function LetsWork() {
           Working with you, we'll create an outstanding web presence. Let's get
           started!
         </h2>
+      </section>
+      <hr className="w-[90%] mx-auto border-2 border-slate-200" />
+      <section className="w-full h-auto min-h-screen p-4">
+        <img
+          src={avatar}
+          alt=""
+          className="w-1/2 sm:w-1/3 md:w-1/4 mt-24 sm:mt-36 md:mt-32 lg:mt-28 absolute right-5 sm:right-7 md:right-14 bg-slate-400 rounded-full p-2"
+        />
+
+        <h1 className="relative title font-medium text-slate-200 pr-1 mb-5 w-auto max-w-3/4 sm:max-w-1/2 md:max-w-2/3 top-[22rem] sm:top-64 md:top-44 text-2xl sm:text-2xl md:text-3xl lg:text-4xl">
+          Have something in mind ? <br />
+          Lets build it Now!
+        </h1>
       </section>
     </div>
   );
